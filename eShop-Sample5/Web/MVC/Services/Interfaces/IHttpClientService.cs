3b560851d7bc700909d9c123aae2ed652364ba08
Dmytro Kulinich <spo1091@i.ua>
2023-08-01 15:16:05 +0300

    Add HW6_4 Kulinich

---
+++ eShop-Sample5/Web/MVC/Services/Interfaces/IHttpClientService.cs
@@ -0,0 +1,10 @@
+using MVC.ViewModels;
+
+namespace MVC.Services.Interfaces;
+
+public interface IHttpClientService
+{
+    Task<TResponse> SendAsync<TResponse, TRequest>(string url, HttpMethod method, TRequest? content);
+
+    Task<TResponse> GetAsync<TResponse>(string url, HttpMethod method);
+}
